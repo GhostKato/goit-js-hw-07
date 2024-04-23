0c2f6a5d0cd1acd0026534a08d72b9d1b8de5860
GhostKato <osadchenko.maksym@gmail.com>
2024-04-23 10:49:13 +0300

    commit4

--- js/task-6.js
+++ js/task-6.js
@@ -4,6 +4,7 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const controlsContainer = document.querySelector(`#controls`);
 const boxContainer = document.querySelector(`#boxes`);
 const createBatton = document.querySelector(`[data-create]`);
 const destroyButton = document.querySelector(`[data-destroy]`);
@@ -27,17 +28,28 @@ function createBoxes(amount) {
   boxContainer.innerHTML = '';
 
   for (let i = 0; i < amount; i++) {
+    
     const box = document.createElement('div');
-     const size = 30 + i * 10;
+    const size = 30 + i * 10;
+    
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor()
     boxContainer.appendChild(box);
+
+    if (amount > 5) { 
+    const boxContainerSize = 550 + (54 * amount);
+      boxContainer.style.width = `${boxContainerSize}px`;
+      controlsContainer.style.width = `${boxContainerSize}px`;
+  }
   }
 }
 
 function destroyBoxes() {
-  boxContainer.innerHTML = '';  
+  boxContainer.innerHTML = '';
+  
+  boxContainer.style.width = `486px`;
+  controlsContainer.style.width = `486px`;
 }
 
 createBatton.addEventListener(`click`, createFunction);
